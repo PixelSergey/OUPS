1c07b1ee5ff8c0bd0ebcb0ed1f3ee74cd65d5e78
PixelSergey <sergey.ichtchenko@gmail.com>
2025-05-23 15:37:35 +0100

    Add a new index page and delivery page

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,5 +6,7 @@ favicon_view = RedirectView.as_view(url="/static/favicon.ico", permanent=True)
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("send", views.send, name="send"),
+    path("deliver", views.deliver, name="deliver"),
     path("favicon.ico", favicon_view),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "index.html")
+
+
+def send(request):
+    return render(request, "send.html")
+
+
+def deliver(request):
+    return render(request, "deliver.html")
